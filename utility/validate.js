4c21ceeeaf55bb5f67c20355f4f117b3831af2b6
Nazeeh2000 <56908732+Nazeeh2000@users.noreply.github.com>
2020-01-08 11:21:52 +0530

    Update validate.js

--- utility/validate.js
+++ utility/validate.js
@@ -1,3 +1,5 @@
+const updateValidReqFields = ['mrp', 'selling_price', 'stock_count', 'local_shipping_charge', 'zonal_shipping_charge', 'national_shipping_charge', 'listing_status', 'procurement_sla', 'procurement_type', 'actual_stock_count']
+
 module.exports = {
     validateListing: (listing) => {
         // TODO: Validation
